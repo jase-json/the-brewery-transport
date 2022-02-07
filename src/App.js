f63c051d8f67b280fba1e8abb5ae9d3413f8761a
Jason <jase.mathieu@gmail.com>
2022-02-07 22:14:45 +1100

    app layout update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+
 import './App.css';
+import snowFlake from '../src/assets/snow.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>The Brewery Transport</div>
+        <img className="snow-logo" alt="snowflake" src={snowFlake} />
       </header>
       <body className="game">
-        |=|
+        <div className="grid-container">
+          <div className="grid-row-one">
+            <div className="grid-item">+</div>
+            <div className="grid-item">+</div>
+            <div className="grid-item">+</div>
+            <div className="grid-item">+</div>
+            <div className="grid-item">+</div>
+            <div className="grid-item">+</div>
+          </div>
+          <div className="grid-row-two">
+            <div className="grid-item">+</div>
+            <div className="grid-item">+</div>
+            <div className="grid-item">+</div>
+            <div className="grid-item">+</div>
+            <div className="grid-item">+</div>
+            <div className="grid-item">+</div>
+          </div>
+        </div>
       </body>
     </div>
   );
